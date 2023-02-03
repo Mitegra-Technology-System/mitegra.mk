fcd3ea7729688fe6c05809d13a3a2e0a5f07f40a
xDeFc0nx <nehartale8@gmail.com>
2023-02-03 16:02:42 +0100

    re making website initial files

---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreservices = [
+  {
+    id: "service-1",
+    imgUrl: "/service-01.png",
+    title: "PLUMBING",
+  },
+  {
+    id: "service-2",
+    imgUrl: "/service-02.png",
+    title: "HEATING",
+  },
+  {
+    id: "service-3",
+    imgUrl: "/service-03.png",
+    title: "AIR CONDITIONING",
+  },
+  {
+    id: "service-4",
+    imgUrl: "/service-04.png",
+    title: "BATHROOM",
+  },
+  {
+    id: "service-5",
+    imgUrl: "/service-05.png",
+    title: "VENTILATION",
+  },
+];
+
+export const startingFeatures = [
+  "Find a service that suits you and you want to enter",
+  "Enter the service by reading basmalah to be safe",
+  "No need to beat around the bush, just stay on the gas and have fun",
+];
+
+export const newFeatures = [
+  {
+    imgUrl: "/vrpano.svg",
+    title: "A new service",
+    subtitle:
+      "we have the latest update with new service for you to try never mind",
+  },
+  {
+    imgUrl: "/headset.svg",
+    title: "More realistic",
+    subtitle:
+      "In the latest update, your eyes are narrow, making the service more realistic than ever",
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: "/service-06.png",
+    title: "The launch of the Metaverse makes Elon musk ketar-ketir",
+    subtitle:
+      "Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.",
+  },
+  {
+    imgUrl: "/service-07.png",
+    title: "7 tips to easily master the madness of the Metaverse",
+    subtitle:
+      "Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum",
+  },
+  {
+    imgUrl: "/service-08.png",
+    title: "With one platform you can explore the whole service virtually",
+    subtitle:
+      "Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem",
+  },
+];
+
+export const socials = [
+  {
+    name: "twitter",
+    url: "/twitter.svg",
+  },
+  {
+    name: "linkedin",
+    url: "/linkedin.svg",
+  },
+  {
+    name: "instagram",
+    url: "/instagram.svg",
+  },
+  {
+    name: "facebook",
+    url: "/facebook.svg",
+  },
+];
