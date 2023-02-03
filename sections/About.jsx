30dbdd7c99ea5b1f9ba695fb3ba48663551cbbe5
xDeFc0nx <nehartale8@gmail.com>
2023-02-03 20:37:39 +0100

    added favicon changed color for arrow-down

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,5 +1,41 @@
 "use client";
 
-const About = () => <section>About section</section>;
+import { motion } from "framer-motion";
+import { TypingText } from "../components";
+import styles from "../styles";
+import { fadeIn, staggerContainer } from "../utils/motion";
+
+const About = () => (
+  <section id="About-Us" className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="| About Mitegra" textStyles="text-center" />
+      <motion.p
+        variants={fadeIn("up", "tween", 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        <span className="font-extrabold text-black">
+          We are a company that constantly monitors the development of new
+          solutions and technologies, as well as world standards and trends we
+          implement it on our daily work. Previous experience of management,
+          acquired over the past 14 years in some from leading companies in
+          Italy, allows the company to specializes in the design and execution
+          of complete technical solutions and installation maintenance.
+        </span>
+      </motion.p>
+      <motion.img
+        variants={fadeIn("up", "tween", 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+    </motion.div>
+  </section>
+);
 
 export default About;
