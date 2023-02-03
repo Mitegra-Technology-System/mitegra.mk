e515611a179bc7e1ea1348bd2f08e7a747db065d
xDeFc0nx <nehartale8@gmail.com>
2023-02-03 20:38:51 +0100

    finished hero section

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,5 +1,37 @@
 "use client";
 
-const Hero = () => <section>Hero section</section>;
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+import { slideIn, staggerContainer, textVariant } from "../utils/motion";
+
+const Hero = () => (
+  <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <div className="flex justify-center items-center flex-col relative z-10">
+        <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
+          Mitegra
+        </motion.h1>
+      </div>
+
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="relative w-full md:-mt-[20px] -mt-[12px]"
+      >
+        <img
+          src="/cover.png"
+          alt="hero_cover"
+          className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
+        />
+      </motion.div>
+    </motion.div>
+  </section>
+);
 
 export default Hero;
