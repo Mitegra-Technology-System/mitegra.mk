1d0b91126a862b47f8c6bf68ddd6e0aa04e38028
xDeFc0nx <nehartale8@gmail.com>
2023-02-04 11:10:35 +0100

    Fixed typo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -30,14 +30,15 @@ const Navbar = () => {
           <a href="#Our-Services">Our Services</a>
         </div>
         <div className="flex md:order-2">
-          <Link href="/contact">
+          <a href="#Contact-Us">
             <button
               type="button"
+              href="#Contact-US"
               className=" hidden md:flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Contact Us
             </button>
-          </Link>
+          </a>
           <button
             type="button"
             className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
