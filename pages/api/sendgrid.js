0bef77e2778deac9b0f0a3090b987f3f40b1fa15
nehar tale <49609699+xDeFc0nx@users.noreply.github.com>
2023-04-19 13:24:59 +0200

    Update sendgrid.js

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -7,7 +7,7 @@ async function sendEmail(req, res) {
 
   try {
     await sendgrid.send({
-      to: "nehartale8@gmail.com", // Your email where you'll receive emails
+      to: "info@mitegra.mk", // Your email where you'll receive emails
       from: "info@mitegra.mk", // your website email address here
       subject: `[Lead from website] : ${req.body.subject}`,
       html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
